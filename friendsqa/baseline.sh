67898feb04e30f9912b83fce47761336f47a35f7
YanLing Li <45173007+0308kkkk@users.noreply.github.com>
2023-04-12 00:33:06 +0800

    Update baseline.sh

--- friendsqa/baseline.sh
+++ friendsqa/baseline.sh
@@ -7,10 +7,10 @@ CUDA_VISIBLE_DEVICES=3 python3 baseline.py \
 	--model_name_or_path /SISDC_GPFS/Home_SE/hy-suda/pre-train_model/electra-large/ \
 	--do_train \
 	--do_eval \
-    --data_dir /SISDC_GPFS/Home_SE/hy-suda/lyl/MPDM/friendsqa/data \
+    	--data_dir /SISDC_GPFS/Home_SE/hy-suda/lyl/MPDM/friendsqa/data \
 	--train_file train.json \
 	--dev_file dev.json \
-    --test_file test.json \
+    	--test_file test.json \
 	--output_dir ./output/electra2/ \
 	--overwrite_output_dir \
 	--per_gpu_train_batch_size 8 \
@@ -18,12 +18,12 @@ CUDA_VISIBLE_DEVICES=3 python3 baseline.py \
 	--per_gpu_eval_batch_size 8 \
 	--num_train_epochs 3 \
 	--learning_rate 4e-6 \
-    --threads 20 \
+    	--threads 20 \
 	--seed 6243 \
 	--do_lower_case \
-    --evaluate_during_training \
+    	--evaluate_during_training \
 	--max_answer_length 30 \
 	--weight_decay 0.01 \
-    --max_seq_length 512 \
+    	--max_seq_length 512 \
 	--fp16 --fp16_opt_level "O2" 
 
