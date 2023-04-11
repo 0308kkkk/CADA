a625e9c7cccde1687d2213ac1a25ff4ed796456e
YanLing Li <45173007+0308kkkk@users.noreply.github.com>
2023-04-12 00:33:47 +0800

    Update baseline.sh

--- molweni/baseline.sh
+++ molweni/baseline.sh
@@ -5,10 +5,10 @@ CUDA_VISIBLE_DEVICES=3 python3 baseline.py \
 	--model_name_or_path /SISDC_GPFS/Home_SE/hy-suda/pre-train_model/electra-large/ \
 	--do_train \
 	--do_eval \
-    --data_dir /SISDC_GPFS/Home_SE/hy-suda/lyl/MPDM/molweni/data \
+    	--data_dir /SISDC_GPFS/Home_SE/hy-suda/lyl/MPDM/molweni/data \
 	--train_file train.json \
 	--dev_file dev.json \
-    --test_file test.json \
+    	--test_file test.json \
 	--output_dir ./output/baseline-electra2/ \
 	--overwrite_output_dir \
 	--per_gpu_train_batch_size 12 \
@@ -16,11 +16,11 @@ CUDA_VISIBLE_DEVICES=3 python3 baseline.py \
 	--per_gpu_eval_batch_size 8 \
 	--num_train_epochs 3 \
 	--learning_rate 1e-5 \
-    --threads 20 \
+    	--threads 20 \
 	--seed 42 \
 	--do_lower_case \
-    --evaluate_during_training \
+    	--evaluate_during_training \
 	--max_answer_length 30 \
 	--weight_decay 0.01 \
-    --max_seq_length 384 \
+    	--max_seq_length 384 \
 	--fp16 --fp16_opt_level "O2" 
